b2b984f894dd28486f71a6de9f462f1091ad83c6
Serge <zef1rqo@gmail.com>
2022-07-24 16:42:44 +0300

    map pizzas.json

---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Categories = () => {
+    
+    const [activeCategory, setActiveCategory] = React.useState(0);
+    const onClickActiveCategory = (index) =>{
+        setActiveCategory(index)
+    }
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    return(
+        <div className="categories">
+            <ul>
+                {categories.map((value, index) => {
+                    return (
+                        <li onClick={() => onClickActiveCategory(index)} className={activeCategory === index ? 'active' : ''}>{value}</li>
+                    )
+                })}
+              
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
+
+//<li onClick={() => onClickActiveCategory(0)} className={activeCategory === 0 ? 'active' : ''}>Все</li>
